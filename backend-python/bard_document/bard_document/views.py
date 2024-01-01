bd6279146c4ff8ddc8939a05f810a4a8c4693125
Maksym <maksym0.yuzva1@gmail.com>
2024-01-01 13:28:39 +0200

    build: implement POST endpoint for file uploading to the server

--- backend-python/bard_document/bard_document/views.py
+++ backend-python/bard_document/bard_document/views.py
@@ -15,15 +15,17 @@ def greeting(request):
 @csrf_exempt
 @require_POST
 def upload_file(request):
+    print(request)
     uploaded_file = request.FILES.get("file")
+    print(uploaded_file)
+    # if uploaded_file:
+    #     try:
+    #          Process the document using the provided code
+    #          processed_result = process_document(uploaded_file)
 
-    if uploaded_file:
-        try:
-            # Process the document using the provided code
-            processed_result = process_document(uploaded_file)
+    #          return JsonResponse({"result": processed_result})
+    #     except Exception as e:
+    #         return JsonResponse({"error": str(e)}, status=500)
 
-            return JsonResponse({"result": processed_result})
-        except Exception as e:
-            return JsonResponse({"error": str(e)}, status=500)
-
-    return JsonResponse({"error": "No file uploaded"}, status=400)
+    # return JsonResponse({"error": "No file uploaded"}, status=400)
+    return JsonResponse({"result": "success"})
--- src/components/UploadButton.tsx
+++ src/components/UploadButton.tsx
@@ -2,7 +2,6 @@
 import { useState } from "react";
 import { Button } from "./ui/button";
 import Dropzone from "react-dropzone";
-import axios from "axios";
 import { File, Loader2 } from "lucide-react";
 import {
   Dialog,
@@ -16,7 +15,65 @@ interface UploadButtonProps {
   file: File | null;
 }
 
-const UploadDropzone = () => {
+// const UploadDropzone = () => {
+//   const { toast } = useToast();
+//   const [isUploading, setIsUploading] = useState<boolean>(false);
+//   const [uploadProgress, setUploadProgress] = useState<number>(0);
+//   const startSimulatedProgress = () => {
+//     setUploadProgress(0);
+//     const interval = setInterval(() => {
+//       setUploadProgress((prevProgress) => {
+//         if (prevProgress >= 95) {
+//           clearInterval(interval);
+//           return prevProgress;
+//         }
+//         return prevProgress + 5;
+//       });
+//     }, 500);
+//     return interval;
+//   };
+//   return (
+//     <Dropzone
+//       multiple={false}
+//       onDrop={async (acceptedFile) => {
+//         setIsUploading(true);
+//         const progressInterval = startSimulatedProgress();
+//         if (acceptedFile.length > 0) {
+//           const file = acceptedFile[0] as File;
+//           try {
+//             const response = await axios.post("/api/upload", file.slice(), {
+//               headers: {
+//                 "Content-Type": file.type,
+//               },
+//             });
+//           } catch (err) {
+//             console.error(err);
+//             toast({
+//               title: "Something went wrong",
+//               description: "Please try again later",
+//               variant: "destructive",
+//             });
+//           } finally {
+//             clearInterval(progressInterval);
+//             setUploadProgress(100);
+//             setIsUploading(false);
+//           }
+//         }
+//       }}
+//     >
+//       {({ getRootProps, getInputProps, acceptedFiles }) => (
+//         <>
+//           <div
+//             {...getRootProps()}
+//             className="border h-64 border-dashed border-gray-300 rounded-lg"
+//           />
+//         </>
+//       )}
+//     </Dropzone>
+//   );
+// };
+
+const UploadButton = ({ file }: UploadButtonProps) => {
   const { toast } = useToast();
   const [isUploading, setIsUploading] = useState<boolean>(false);
   const [uploadProgress, setUploadProgress] = useState<number>(0);
@@ -33,82 +90,86 @@ const UploadDropzone = () => {
     }, 500);
     return interval;
   };
-  return (
-    <Dropzone
-      multiple={false}
-      onDrop={async (acceptedFile) => {
-        setIsUploading(true);
-        const progressInterval = startSimulatedProgress();
-        if (acceptedFile.length > 0) {
-          const file = acceptedFile[0] as File;
-          try {
-            const response = await axios.post("/api/upload", file.slice(), {
-              headers: {
-                "Content-Type": file.type,
-              },
-            });
-          } catch (err) {
-            console.error(err);
-            toast({
-              title: "Something went wrong",
-              description: "Please try again later",
-              variant: "destructive",
-            });
-          } finally {
-            clearInterval(progressInterval);
-            setUploadProgress(100);
-            setIsUploading(false);
-          }
-        }
-      }}
-    >
-      {({ getRootProps, getInputProps, acceptedFiles }) => (
-        <>
-          <div
-            {...getRootProps()}
-            className="border h-64 border-dashed border-gray-300 rounded-lg"
-          />
-        </>
-      )}
-    </Dropzone>
-  );
-};
 
-const UploadButton = ({ file }: UploadButtonProps) => {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const handleUpload = async ({ file }: UploadButtonProps) => {
+    if (!file) {
+      // Handle case where no file is selected
+      toast({
+        title: "No File Selected",
+        description: "Please select a file before uploading.",
+        variant: "destructive",
+      });
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append("file", file);
+
+    const progressInterval = startSimulatedProgress();
+    try {
+      setIsUploading(true);
+
+      const response = await fetch("http://127.0.0.1:8000/api/upload", {
+        method: "POST",
+        body: formData,
+        headers: {
+          // Customize headers based on your server requirements
+          // "Content-Type": "multipart/form-data", // Fetch API automatically sets this for FormData
+        },
+      });
+      if (response.ok) {
+        const responseData = await response.json();
+
+        // Handle successful upload
+        console.log("Upload successful", responseData);
+
+        // Optionally show a success toast
+        toast({
+          title: "Upload Successful",
+          description: "Your file has been successfully uploaded.",
+          variant: "default",
+        });
+      }
+    } catch (error) {
+      // Handle upload error
+      console.error("Upload error", error);
+
+      // Show an error toast
+      toast({
+        title: "Upload Failed",
+        description:
+          "Something went wrong during the upload. Please try again.",
+        variant: "destructive",
+      });
+    } finally {
+      clearInterval(progressInterval);
+      setUploadProgress(100);
+      setIsUploading(false);
+    }
+  };
+  const handleButtonClick = () => {
+    handleUpload({ file });
+  };
   return (
-    <Dialog
-      open={isOpen}
-      onOpenChange={(v) => {
-        if (!v) {
-          setIsOpen(v);
-        }
-      }}
-    >
-      <DialogTrigger onClick={() => setIsOpen(true)} asChild>
-        <div className="w-full flex justify-center">
-          {file ? (
-            <Button
-              className="text-zinc-100 bg-blue-500  hover:bg-blue-600"
-              size={"sm"}
-            >
-              Submit
-            </Button>
-          ) : (
-            <Button
-              className="text-zinc-100 bg-blue-500  hover:bg-blue-600"
-              size={"sm"}
-              disabled
-            >
-              Upload Word file
-            </Button>
-          )}
-        </div>
-      </DialogTrigger>
-      <DialogContent>
-        <UploadDropzone />
-      </DialogContent>
-    </Dialog>
+    <div className="w-full flex justify-center">
+      {file ? (
+        <Button
+          className="text-zinc-100 bg-blue-500  hover:bg-blue-600"
+          size={"sm"}
+          onClick={handleButtonClick}
+        >
+          {isUploading ? <Loader2 className="animate-spin mr-2" /> : "Submit"}
+        </Button>
+      ) : (
+        <Button
+          className="text-zinc-100 bg-blue-500  hover:bg-blue-600"
+          size={"sm"}
+          disabled
+        >
+          Upload Word file
+        </Button>
+      )}
+    </div>
   );
 };
 
