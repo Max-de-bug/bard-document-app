265bc1ec41a7ee86238a4df151d6ac489a27b5b6
Maksym <maksym0.yuzva1@gmail.com>
2024-01-05 15:38:27 +0200

    build: implement file validation on the server

--- backend-python/bard_document/bard_document/views.py
+++ backend-python/bard_document/bard_document/views.py
@@ -1,3 +1,5 @@
+from django.core.validators import FileExtensionValidator, MaxValueValidator
+from django.forms import ValidationError
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
@@ -18,6 +20,23 @@ def upload_file(request):
     print(request)
     uploaded_file = request.FILES.get("file")
     print(uploaded_file)
+    if not upload_file:
+        return JsonResponse({"error": "No file uploaded"}, status=400)
+
+    # validate file extention
+    validator = FileExtensionValidator(allowed_extensions=["docx"])
+    try:
+        validator(uploaded_file)
+    except ValidationError as e:
+        return JsonResponse({"error": f"Invalid file format: {e}"}, status=400)
+
+    max_size_validator = MaxValueValidator(limit_value=4 * 1024 * 1024)
+    try:
+        max_size_validator(uploaded_file.size)
+    except ValidationError as e:
+        return JsonResponse(
+            {"error": f"File size exceeds the allowed limit: {e}"}, status=400
+        )
     # if uploaded_file:
     #     try:
     #          Process the document using the provided code
